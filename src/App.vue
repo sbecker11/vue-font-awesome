2b3568373571e13838381fb130eb311c0455ec01
Shawn Becker <sbecker11@me.com>
2023-04-16 03:29:42 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Getting Started with Vue.js and Font Awesome</h1>
+  <p>Have a phone call: <fa icon="phone" /></p>
+
+  <div id="app">
+    <h1>Using Font Awesome "Brand" icons in Vue.js</h1>
+    <p>Have a phone call: <fa icon="phone-volume" /></p>
+    <p>Have a module of JavaScript: <fa icon="fa-js" /></p>
+    <p>Have a module of Vue: <fa icon="fa-vue-js" /></p>
+    <p>Have a briefcase medial: <fa icon="fa-briefcase-medical" /></p>
+    <p>Have a bacteria: <fa icon="fa-bacteria" /></p>
+    <p>Have a module of Secret: <fa icon="fa-user-secret" /></p>
+    <p>Have a user clock: <fa icon="fa-user-clock" /></p>
+    <p>Have a clock four: <fa icon="fa-clock-four" /></p>
+    <p>Have a water: <fa icon="fa-water" /></p>
+    <p>Have a X-mark: <fa icon="fa-xmark" size="2xl" style ="color: #ff0000;" /></p>
+    <p>Have a check-circle: <fa icon="fa-check-circle" /></p>
+    <p>Have a check: <fa icon="fa-check" size="2xl" style ="color: #008800;" /></p>
+    <p>When my six o’clock alarm buzzes, I require a pot of good java <fa icon="fa-coffee" /> </p>
+    <p class="fa-brands fa-shopify">Shoppify</p>
+  </div>
+
+  
+  <!-- solid style -->
+ <p>A solid user: <fa icon="fas-solid fa-user" /></p>
+ <p>A regular user: <fa icon="far-regular fa-user" /></p>
+ <p>A light user: <fa icon="fal-light fa-user" /></p>
+ <p>A duotone large user: <fa icon="fad-duotone fa-user" size="lg" /></p>
+ <p>A 2xl users: <fa icon="fa-users" size="2xl" style ="color: #ff0000;"/></p>
+
+
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { 
+    faPhone, faPhoneVolume, 
+    faUserSecret, faBriefcaseMedical, 
+    faUserClock, faBacteria, faClockFour, 
+    faCoffee, faWater, faXmark, faCheck, 
+    faUser, faCheckCircle, faUsers
+} from "@fortawesome/free-solid-svg-icons";
+// import { faJs, faVuejs } from "@fortawesome/free-brands-svg-icons";
+// import { faThin } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).mount('#app')
+library.add(faPhone, faPhoneVolume, 
+    faUserSecret, faUserClock, faCoffee,
+    faBriefcaseMedical, faBacteria, faClockFour, 
+    faWater, faXmark, faCheck, faUser,
+    faCheckCircle, faUsers
+    // faJs, faVuejs
+);
+
+createApp(App).component("fa", FontAwesomeIcon).mount("#app");
